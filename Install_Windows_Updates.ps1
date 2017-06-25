ce6c55ecc48236478a353e8524d015fb2951e97a
clhill312 <clhill312@gmail.com>
2017-06-25 14:13:19 -0400

    added info

--- Install_Windows_Updates.ps1
+++ Install_Windows_Updates.ps1
@@ -1,11 +1,22 @@
+#Requires -RunAsAdministrator
 
+<#
+  .DESCRIPTION
+    Offline update (.msu files) installer.
+
+  .NOTES
+    Author: Carl Hill
+    Contact: carl.l.hill@outlook.com
+
+#>
+
+# Start Variables
 $updatedir = "\\server\share\folder\updates"
+$I = 1 #interval
+# End Variables
 
-$I = 1
-$updatecount = (Get-ChildItem -Path $updatedir | Measure-Object).Count
-
-Get-ChildItem -Path $updatedir | ForEach-Object {
-    Write-Host "Installing update" $I "of" $updatecount ":" $_.Name
+Get-ChildItem -Path $updatedir| ForEach-Object {
+    Write-Host "Installing update" $I "of" (gci).Count ":" $_.Name
     wusa.exe $_.Name /quiet /norestart
     $I++
 }
