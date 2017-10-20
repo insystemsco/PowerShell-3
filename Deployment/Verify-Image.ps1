687b6c0b70c4893ac8a4be6bd6805eded951590e
Carl Hill <clhill312@users.noreply.github.com>
2017-10-20 16:16:51 +0200

    Update Verify-Image.ps1

--- Deployment/Verify-Image.ps1
+++ Deployment/Verify-Image.ps1
@@ -1,8 +1,8 @@
- #Requires -RunAsAdministrator 
+#Requires -RunAsAdministrator 
 
 <# 
    .DESCRIPTION 
-     Validates and corrects deficiencies for Windows 10 AGM image. 
+     Validates and corrects deficiencies for Windows 10 image. 
   
    .NOTES 
      Author: Carl Hill 
@@ -10,16 +10,15 @@
 
    .CHANGELOG
     2017-10-10
-        Added check for group membership
+        Removed the creation of Temp folder IF statements
     2017-10-6
         Added checks for TPM & UEFI
     2017-09-27
         Narrowed list of apps to essential apps
         Transferred app list to arrary in script instead of in apps.txt
-        Removed Install All SCCM Apps function
+        Commeneted out Install All SCCM Apps function
 
     2017-08-24
-        Replaced google.com with local domain for ping test
         Replaced Write-Host with Write-Verbose/Error/Warning/Output
         Simplified Countdown mechanism
         Added BitLocker HDD encryption detection
@@ -34,15 +33,13 @@ $logpath = "\\server\share\folder\logs"
 $date = Get-Date -Format yyyy-MM-dd_HHmmss
 [string]$logfile =  "Image Validation - $($env:COMPUTERNAME) - $($date).txt"
 
-　
+
 Start-Transcript -Path $logpath\$logfile -Append
 
-　
-　
-　
-# TPM status
 
-　
+
+
+# TPM status
 $TPMStatus = wmic /namespace:\\root\cimv2\security\microsofttpm path win32_tpm get IsActivated_InitialValue
 
 if ($TPMStatus) {
@@ -53,10 +50,8 @@ else {
     $errors++
     }
 
-　
-# Check for UEFI 
 
-　
+# Check for UEFI 
 Function Get-BiosType {
 
 [OutputType([UInt32])]
@@ -83,70 +78,58 @@ Add-Type -Language CSharp -TypeDefinition @'
     }
 '@
 
-　
+
     [FirmwareType]::GetFirmwareType()
 }
 
-　
+
 Switch (Get-BiosType) {
     1       {Write-Error "Firmware Type is Legacy BIOS";$errors++}
     2       {Write-Output "Firmware is UEFI"}
     Default {Write-Error "Firmware Type is Unknown";$errors++}
     }
 
-　
-# check if member of quarantine group
 
-if ((Get-ADComputer $env:ComputerName -Properties memberof) -match "GROUP1" -or "GROUP2") {
-    Write-Error "Computer is in GROUP" -Category ResourceExists -RecommendedAction "SCCM Repair"
-    $errors++
-    }
-    else {
-        Write-Output "Computer is not in GROUP"
-        }
 
-　
-　
-　
-# pings local domain to verify network functionality
 
-    if (Test-Connection -Count 1 domain.local) {
-        Write-Output "Network connection to domain.local is successful. `n"
+# pings google.com to verify network functionality
+
+    if (Test-Connection -Count 1 google.com) {
+        Write-Output "Network connection to google.com is successful. `n"
         }
 
         else {
-            Write-Warning "Network connection to domain.local NOT successful! `n"
+            Write-Warning "Network connection to google.com NOT successful! `n"
             }
 
-　
-　
-　
-　
-# Checks to see if the Portal page is added into the Site to Zone settings, and adds them if they are not
 
-$portalHKLM1 = Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.domain.local" -ErrorAction SilentlyContinue
-$portalHKLM2 = Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.sub.domain.local" -ErrorAction SilentlyContinue
 
-    if ($portalHKLM1.'*.domain.local' -eq 1 -and $portalHKLM2.'*.sub.domain.local' -eq 1) {
-        Write-Output "Portal page Site to Zone has been added to Local Group Policy."
+
+
+# Checks to see if page is added into the Site to Zone settings, and adds them if they are not
+
+$portalHKLM1 = Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.google.com" -ErrorAction SilentlyContinue
+$portalHKLM2 = Get-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.maps.google.com" -ErrorAction SilentlyContinue
+
+    if ($portalHKLM1.'*.google.com' -eq 1 -and $portalHKLM2.'*.maps.google.com' -eq 1) {
+        Write-Output "Site to Zone has been added to Local Group Policy."
     }
 
-　
+
     else {
-        Write-Warning "Portal page Site to Zone has NOT been added to Local Group Policy!"
-        Write-Verbose "The Portal page will now be added to the Site to Zone Local Group Policy.."
+        Write-Warning "Site to Zone has NOT been added to Local Group Policy!"
+        Write-Verbose "The page will now be added to the Site to Zone Local Group Policy.."
 
-        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.domain.local" -PropertyType String -Value 1
-        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.sub.domain.local" -PropertyType String -Value 1
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.google.com" -PropertyType String -Value 1
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMapKey" -Name "*.maps.google.com" -PropertyType String -Value 1
 
     }
 
-　
-　
-　
-　
- #determines if the computer is a laptop and installs VPN client if it is a laptop
+    gpupdate /force
 
+
+
+# determines if the computer is a laptop and installs VPN if it is a laptop
 Function Get-Laptop
 {
  Param(
@@ -162,35 +145,24 @@ Function Get-Laptop
  $isLaptop
 } 
 
-　
-　
+
+
 if (Get-Laptop) {
-    Write-Output "Computer is a laptop; will install VPN client..."
+    Write-Output "Computer is a laptop; will install  VPN client..."
 
-    if (Test-Path C:\Temp) {
-        Write-Output "Temp folder found..."
-        }
-    else {
-        Write-Output "Temp folder not found, creating Temp folder..."
-        mkdir C:\Temp
-        }
-
-　
-    Copy-Item '\\server\share\folder\vpnclient.exe' 'C:\Temp'
-    Start-Process  'C:\Temp\vpnclient.exe' -Wait
+    Copy-Item '\\server\share\folder\vpn.exe' 'C:\Temp'
+    Start-Process  'C:\Temp\vpn.exe' -Wait
     }
 else {
     Write-Output "Computer is not a laptop, skipping VPN install..."
     } 
 
-　
-　
-　
-　
+
+
 # Checks SCCM Software Center for available updates
 $AvailableUpdates = Get-CimInstance -Namespace root\ccm\clientsdk -Query 'Select * from CCM_SoftwareUpdate'
 
-　
+
 Write-Output "$($AvailableUpdates.Count) Updates Pending..."
 
 foreach ($update in $AvailableUpdates) {
@@ -224,7 +196,7 @@ foreach ($update in $AvailableUpdates) {
     }
 }
 
-　
+
 # check to see if the computer needs a reboot
 if ((icim -namespace root\ccm\clientsdk -ClassName CCM_ClientUtilities -Name DetermineIfRebootPending).RebootPending) {
     Write-Output "Computer Requires Reboot from installed updates."
@@ -239,30 +211,22 @@ if ((icim -namespace root\ccm\clientsdk -ClassName CCM_ClientUtilities -Name Det
     Write-Output "Updates are installed, no reboot necessary."
     }
 
-　
-　
-　
+
+
+
 # Installs McAfee Agent if not installed.
 if (Test-Path "C:\Program Files\McAfee\Agent") {
     Write-Verbose "McAfee Agent is installed."
 }
 else {
-    
-    if (Test-Path 'C:\Temp') {
-        Write-Verbose "Temp folder found..."
-        }
-    else {
-        Write-Verbose "Temp folder not found, creating Temp folder..."
-        mkdir 'C:\Temp'
-        }
-        
+            
     Write-Verbose "McAfee Agent not installed. Installing McAfee Agent..."
-    Copy-Item '\\server\share\folder\agent.exe' 'C:\Temp'
-    Start-Process 'C:\Temp\agent.exe' -ArgumentList /install=agent, /s, /forceinstall -Wait
+    Copy-Item '\\server\share\mcafee' 'C:\Temp'
+    Start-Process 'C:\Temp\mcafee.exe' -ArgumentList /install=agent, /s, /forceinstall -Wait
     Write-Output 'McAfee Agent installtion complete.'
 }
 
-　
+
 function Is-Installed ($program) {
 # Checks to see what applications are installed.
     
@@ -275,9 +239,9 @@ function Is-Installed ($program) {
     return $x86 -or $x64;
 }
 
-　
-　
-　
+
+
+
 $applist = @(
 "McAfee Agent",
 "Microsoft Office",
@@ -286,12 +250,12 @@ $applist = @(
 "Microsoft S/MIME"
  )
 
-　
+
 Write-Verbose "Checking for installed applications..."
 
 foreach ($program in $applist) { 
 
-　
+
     if (Is-Installed -program $program) {
         Write-Output "$program is installed."
         }
@@ -302,8 +266,8 @@ foreach ($program in $applist) {
 
 }
 
-　
-# Check BitLocker Status
+
+# Check BitLocker Conversion Status
 # Win32_EncryptableVolume class at MSDN:
 # https://msdn.microsoft.com/en-us/library/windows/desktop/aa376483(v=vs.85).aspx
 
@@ -319,26 +283,50 @@ Switch ($BLStatus.ConversionStatus) {
     default {Write-Warning "Unknown BitLocker status!"; $errors++; break}
 }
 
-　
-　
+
+# Check BitLocker Protectin Status
+
+function ReenableBitlocker {
+# ejects CD
+$Eject = New-Object -ComObject "Shell.Application"
+$Eject.Namespace(17).Items() |
+    Where-Object { $_.Type -eq "CD Drive" } |
+        foreach { $_.InvokeVerb("Eject") } 
+
+# enables protectors
+manage-bde -protectors -enable C:
+
+# Changes PIN
+Add-bitlockerkeyprotector -mountpoint "C:" -pin (ConvertTo-SecureString "1234567890" -AsPlainText -Force) -tpmandpinpr
+otector
+ }
+
+$ProtectionState = Get-WmiObject -Namespace ROOT\CIMV2\Security\Microsoftvolumeencryption -Class Win32_encryptablevolume -Filter "DriveLetter = 'c:'"
+
+Switch ($ProtectionState.ProtectionStatus) {
+    0 {Write-Error "HDD is Unprotected"; ReenableBitLocker; break}
+    1 {Write-Output "HDD is Protected"; break}
+    2 {"HDD Protection is Unknown"; $errors++; break}
+    default {"Unable to find BitLocker proteciton state"; break}
+}
+
+
+
+
+
 Write-Output "Script finished with $errors errors."
 
-　
-　
+
+
 #  script results
 
 if ($errors -eq "0") {Write-Output "[FINAL] The image has been validated successfully!"}
 
 else {
-    Write-Output -ForegroundColor Red "[FINAL] Image Validation Failed!"
+    Write-Output "[FINAL] Image Validation Failed!"
     }
 
-　
-　
-　
-Stop-Transcript
 
-　
-　
-　
-        
+
+
+Stop-Transcript
