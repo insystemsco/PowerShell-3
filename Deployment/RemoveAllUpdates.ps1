668caebc92507724713d1c89b666eb0934b61841
Carl Hill <clhill312@users.noreply.github.com>
2018-05-13 01:20:41 +0200

    Update RemoveAllUpdates.ps1

--- Deployment/RemoveAllUpdates.ps1
+++ Deployment/RemoveAllUpdates.ps1
@@ -1,5 +1,6 @@
 $updates = Get-HotFix
 
 foreach ($update in $updates) {
+    Write-Output "Uninstalling $($update.HotFixID)"
     Start-Process wusa.exe -ArgumentList /uninstall "/kb:$(($update.HotFixID).Substring(2))", /quiet, /norestart -Wait
 }
